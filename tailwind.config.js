03a9521cb6afa1f4e3896b546e918fd9ca107bd2
Chuks <63956312+skido600@users.noreply.github.com>
2025-01-27 21:17:44 +0100

    1 payment changed color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,js,jsx}", // Scans all HTML, JS, and JSX files inside the src directory
-    "./index.html", // Scans the index.html file
-    "./script/**/*.js", // Corrected glob pattern for all JS files in the script directory
-  ],
+  content: ["./src/**/*.{html,js,jsx}", "./index.html", "./script/**/*.js"],
   theme: {
     extend: {
       fontFamily: {
