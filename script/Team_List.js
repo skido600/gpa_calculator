94e5aa05ec1b5d5fee85148f1431b36733b0b70d
Leo  wave <63956312+skido600@users.noreply.github.com>
2025-02-03 16:14:50 +0100

    Update Team_List.js payment

--- script/Team_List.js
+++ script/Team_List.js
@@ -3,7 +3,8 @@ export function setupTeams() {
     { Name: "Ebisi Chinecherem Leonard", Reg: "NS/CSC/22/7428" },
     { Name: "Uzoka PraiseGod Chimpuluime", Reg: "NS/CSC/22/7494" },
     { Name: "Orji Bethel Chidera", Reg: "NS/CSC/24/9154" },
-    {Name:"Sunday Jennifer Osemhenkhian", Reg: "NS/CSC/22/7545"}
+    {Name:"Sunday Jennifer Osemhenkhian", Reg: "NS/CSC/22/7545"},
+    {Name:"Oyibo Nwachukwu",Reg:"Ns/csc/23/8284"},
   ];
 
   const teamMain = document.getElementById("teams");
